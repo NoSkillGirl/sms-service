24fd0b7fe0ff95265ed545334651064e2d7b76b7
NoSkillGirl <singhpooja240393@gmail.com>
2019-12-31 17:44:46 +0530

    sms json post request changes

--- api/api.go
+++ api/api.go
@@ -2,22 +2,49 @@ package api
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"bitbucket.org/lightbulbwithswitch/sms-service.git/twillio"
 	_ "github.com/go-sql-driver/mysql"
 )
 
+// SendSMSRequest - stuct
+type SendSMSRequest struct {
+	Message string
+	Mobile  string
+}
+
+// SendSMSResponse - struct
+type SendSMSResponse struct {
+	Message string
+}
+
 // SendSMS - api send sms
 func SendSMS(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
-		mobileNumber := "+91" + r.FormValue("mobile")
-		messageToSend := r.FormValue("message")
-		fmt.Printf("Mobile number is: %s\nMessage to send is: %s\n", mobileNumber, messageToSend)
+		sendSMSRequest := SendSMSRequest{}
+
+		jsn, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			fmt.Println("Error in reading request body", err)
+		}
+
+		err = json.Unmarshal(jsn, &sendSMSRequest)
+		if err != nil {
+			fmt.Println("Decoding Error", err)
+		}
+
+		fmt.Println("sendSMSRequest ....")
+		fmt.Println(sendSMSRequest)
+
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Printf("Mobile number is: %s\nMessage to send is: %s\n", sendSMSRequest.Mobile, sendSMSRequest.Message)
 
 		//sending message through twillio
-		respStatus := twillio.SendSMS(mobileNumber, messageToSend)
+		respStatus := twillio.SendSMS(sendSMSRequest.Mobile, sendSMSRequest.Message)
 		fmt.Println(respStatus)
 
 		if respStatus != 201 {
@@ -25,31 +52,28 @@ func SendSMS(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
+		// initialize db connection
 		db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/smsDetails")
-
-		// if there is an error opening the connection, handle it
 		if err != nil {
-			panic(err.Error())
+			fmt.Println(err)
 		}
-
-		// defer the close till after the main function has finished
-		// executing
 		defer db.Close()
 
 		smsInsertQuery := `insert into messages (mobile, message, delivery_status, provider) values ('%s', '%s', 'SENT', 'TWILLIO')`
-
-		smsInsertQueryString := fmt.Sprintf(smsInsertQuery, mobileNumber, messageToSend)
+		smsInsertQueryString := fmt.Sprintf(smsInsertQuery, sendSMSRequest.Mobile, sendSMSRequest.Message)
 		fmt.Println(smsInsertQueryString)
 
 		// perform a db.Query insert
 		insert, err := db.Query(smsInsertQueryString)
-
-		// if there is an error inserting, handle it
 		if err != nil {
 			panic(err.Error())
 		}
-		// be careful deferring Queries if you are using transactions
 		defer insert.Close()
-		fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+
+		sendSMSResponse := SendSMSResponse{
+			Message: "SMS Sent Successfull",
+		}
+
+		json.NewEncoder(w).Encode(sendSMSResponse)
 	}
 }
